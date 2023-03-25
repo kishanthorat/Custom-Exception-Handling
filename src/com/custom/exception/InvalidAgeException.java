ff76d9e0b1100d2922b823a9b667281ea58e3a0b
User <kishanthorat5@gmail.com>
2023-03-25 10:50:06 +0530

    Custom Exception Handling

---
+++ src/com/custom/exception/InvalidAgeException.java
@@ -0,0 +1,10 @@
+package com.custom.exception;
+
+public class InvalidAgeException extends Exception {
+	
+	public InvalidAgeException(String msg)
+	{
+		super(msg);
+	}
+
+}
---
+++ src/com/custom/exception/Test.java
@@ -0,0 +1,24 @@
+package com.custom.exception;
+
+public class Test {
+	
+	public void m1(int age) throws InvalidAgeException
+	{
+		if(age<=0)
+			 throw new InvalidAgeException("Age Invalid");
+	}
+	
+	public static void main(String[] args) {
+		
+		Test t=new Test();
+		try {
+		   t.m1(-5);
+		}
+		catch (InvalidAgeException e) {
+//		   e.printStackTrace();
+			System.out.println("Exception "+e);
+		}
+		System.out.println("Done");
+	}
+
+}
